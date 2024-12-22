19704d580efd655a93c0df0f7c1e60309eebbf7f
aayampokharel <aayam.pokharel@gmail.com>
2024-12-22 16:25:58 +0545

    added map for defining uris for different tabs in my programming language

---
+++ lib/map_for_post.dart
@@ -0,0 +1,17 @@
+Map<String, Map<String, String>> mapForPost = {
+  "Photo": {
+    "uri": "http://localhost:8080/uploadphotos",
+  },
+  "Video": {
+    "uri": "http://localhost:8080/uploadvideos",
+  },
+  "Audio": {
+    "uri": "http://localhost:8080/uploadaudios",
+  },
+  "Text": {
+    "uri": "http://localhost:8080/uploadtexts",
+  },
+  "PDF": {
+    "uri": "http://localhost:8080/uploadpdfs",
+  },
+};
